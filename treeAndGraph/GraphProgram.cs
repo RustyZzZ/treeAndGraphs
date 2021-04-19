b358d35f3969009c1dd1c362df4c42486f7ef03a
Rostyslav <rdiachuk92@gmail.com>
2021-04-19 19:13:38 +0300

    Initial commit

---
+++ treeAndGraph/GraphProgram.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace treeAndGraph
+{
+    public class GraphProgram
+    {
+        public static void Main(string[] args)
+        {
+            var graph = new Graph(new int[,]
+            {
+                {0, 0, 12, 0, 1},
+                {0, 34, 0, 11, 1},
+                {10, 0, 0, 0, 0},
+                {21, 31, 0, 0, 0},
+                {11, 35, 14, 12, 12},
+            }, 5);
+           // var bft = graph.breadthFirstTraverse("0");
+            Console.WriteLine(graph.shortestPathDijkstra("0", "3"));
+           //
+        }
+
+        class Vertex
+        {
+            public string value;
+
+            public Vertex(string value)
+            {
+                this.value = value;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return value.Equals(((Vertex) obj).value);
+            }
+
+            public override int GetHashCode()
+            {
+                return 20 + value.GetHashCode();
+            }
+        }
+
+        class Edge
+        {
+            public Vertex to;
+            public int weight;
+
+            public Edge(Vertex to, int weight)
+            {
+                this.to = to;
+                this.weight = weight;
+            }
+        }
+
+        class Graph
+        {
+            private Dictionary<Vertex, List<Edge>> edges;
+            private List<VertexInfo> infos;
+
+            public Graph()
+            {
+                edges = new Dictionary<Vertex, List<Edge>>();
+            }
+
+            public Graph(int[,] adjacency, int n)
+            {
+                edges = new Dictionary<Vertex, List<Edge>>();
+                for (int i = 0; i < n; i++)
+                {
+                    var edgeList = new List<Edge>();
+                    var vertex = new Vertex($"{i}");
+                    for (int j = 0; j < n; j++)
+                    {
+                        var weigth = adjacency[i, j];
+                        if (weigth > 0)
+                        {
+                            edgeList.Add(new Edge(new Vertex($"{j}"), weigth));
+                        }
+                    }
+
+                    edges.Add(vertex, edgeList);
+                }
+            }
+
+            public void AddVertex(string value)
+            {
+                var vertex = new Vertex(value);
+                edges.Add(vertex, new List<Edge>());
+            }
+
+            public void AddEdge(string v1, string v2)
+            {
+                addEdge(v1, v2, 1);
+            }
+
+            public void addEdge(string v1, string v2, int weight)
+            {
+                var vertex1 = new Vertex(v1);
+                var vertex2 = new Vertex(v2);
+                if (edges.ContainsKey(vertex1) && edges.ContainsKey(vertex2))
+                {
+                    edges[vertex1].Add(new Edge(vertex2, weight));
+                }
+            }
+
+
+            public List<String> depthFirstTraverse(string root)
+            {
+                if (!edges.ContainsKey(new Vertex(root)))
+                {
+                    throw new Exception("No such root in graph");
+                }
+
+                var visited = new List<string>();
+                var stack = new Stack<string>();
+
+                stack.Push(root);
+                while (stack.Count != 0)
+                {
+                    var vertexName = stack.Pop();
+                    if (!visited.Contains(vertexName))
+                    {
+                        visited.Add(vertexName);
+                        foreach (var v in edges[new Vertex(vertexName)])
+                        {
+                            stack.Push(v.to.value);
+                        }
+                    }
+                }
+
+                return visited;
+            }
+
+            public List<String> breadthFirstTraverse(string root)
+            {
+                if (!edges.ContainsKey(new Vertex(root)))
+                {
+                    throw new Exception("No such root in graph");
+                }
+
+                var visited = new List<string>();
+                var queue = new Queue<string>();
+
+                queue.Enqueue(root);
+                while (queue.Count != 0)
+                {
+                    var vertexName = queue.Dequeue();
+                    if (!visited.Contains(vertexName))
+                    {
+                        visited.Add(vertexName);
+                        foreach (var v in edges[new Vertex(vertexName)])
+                        {
+                            queue.Enqueue(v.to.value);
+                        }
+                    }
+                }
+
+                return visited;
+            }
+
+            public string breadthFirstSearch(string root, string searchValue)
+            {
+                if (!edges.ContainsKey(new Vertex(root)))
+                {
+                    throw new Exception("No such root in graph");
+                }
+
+                var visited = new List<string>();
+                var queue = new Queue<string>();
+
+                queue.Enqueue(root);
+                while (queue.Count != 0)
+                {
+                    var vertexName = queue.Dequeue();
+                    if (!visited.Contains(vertexName))
+                    {
+                        visited.Add(vertexName);
+                        foreach (var v in edges[new Vertex(vertexName)])
+                        {
+                            if (v.to.value == searchValue)
+                            {
+                                return v.to.value;
+                            }
+
+                            queue.Enqueue(v.to.value);
+                        }
+                    }
+                }
+
+                return "No such value";
+            }
+
+            public string shortestPathDijkstra(string v1, string v2)
+            {
+                infos = new List<VertexInfo>();
+                foreach (var ver in edges.Keys)
+                {
+                    infos.Add(new VertexInfo(ver));
+                }
+
+                var ver1 = new Vertex(v1);
+                var ver2 = new Vertex(v2);
+                var infoV1 = getInfoForVertex(ver1);
+                infoV1.weightSum = 0;
+
+                var current = findUnvisitedWithMinSum();
+
+                while (current != null)
+                {
+                    setSumToNextElement(current);
+                    current = findUnvisitedWithMinSum();
+                }
+
+                return getPath(ver1, ver2);
+            }
+
+            private string getPath(Vertex ver1, Vertex ver2)
+            {
+                var path = ver2.value;
+                while (ver1 != ver2)
+                {
+                    ver2 = getInfoForVertex(ver2).previous;
+                    path = ver2.value + " " + path;
+                }
+
+                return path;
+            }
+            
+            private void setSumToNextElement(VertexInfo info)
+            {
+                info.isUnvisited = false;
+                foreach (var edge in edges[info.vertex])
+                {
+                    var nextInfo = getInfoForVertex(edge.to);
+                    var sum = info.weightSum + edge.weight;
+                    if (sum < nextInfo.weightSum)
+                    {
+                        nextInfo.weightSum = sum;
+                        nextInfo.previous = info.vertex;
+                    }
+                }
+            }
+
+            private VertexInfo getInfoForVertex(Vertex ver1)
+            {
+                foreach (var v in infos)
+                {
+                    if (v.vertex.Equals(ver1))
+                    {
+                        return v;
+                    }
+                }
+
+                return null;
+            }
+
+            private VertexInfo findUnvisitedWithMinSum()
+            {
+                var minValue = int.MaxValue;
+                VertexInfo minVI = null;
+                foreach (var vertexInfo in infos)
+                {
+                    if (vertexInfo.isUnvisited && vertexInfo.weightSum < minValue)
+                    {
+                        minVI = vertexInfo;
+                        minValue = vertexInfo.weightSum;
+                    }
+                }
+
+                return minVI;
+            }
+        }
+
+        class VertexInfo
+        {
+            public Vertex vertex;
+            public bool isUnvisited;
+            public int weightSum;
+            public Vertex previous;
+
+            public VertexInfo(Vertex v)
+            {
+                vertex = v;
+                isUnvisited = true;
+                weightSum = int.MaxValue;
+                previous = null;
+            }
+        }
+    }
+}
---
+++ treeAndGraph/Program.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace treeAndGraph
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            var tree = new Tree(25);
+            tree.Add(15);
+            tree.Add(50);
+            tree.Add(10);
+            tree.Add(22);
+            tree.Add(35);
+            tree.Add(70);
+            tree.Add(4);
+            tree.Add(12);
+            tree.Add(18);
+            tree.Add(24);
+            tree.Add(31);
+            tree.Add(44);
+            tree.Add(66);
+            tree.Add(90);
+
+           
+           tree.printInorder();
+           Console.WriteLine();
+           tree.printPreorder();
+           Console.WriteLine();
+           tree.printPostorder();
+           Console.WriteLine();
+           tree.printInBreadthFirst();  
+           Console.WriteLine();
+           tree.pringInBreadthRec();
+           Console.WriteLine();
+           tree.printInDepthFirst();
+        }
+    }
+
+    class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int value, TreeNode left, TreeNode right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    class Tree: IComparable
+    {
+        TreeNode root;
+        public Tree(int value)
+        {
+            root = new TreeNode(value);
+        }
+
+        public void addLeft(int value)
+        {
+            var current = root;
+            while (current.left != null)
+            {
+                current = current.left;
+            }
+            current.left = new TreeNode(value);
+        }
+
+        private TreeNode Add(TreeNode current, int value)
+        {
+            if (current == null)
+            {
+                return new TreeNode(value);
+            }
+
+            if (value < current.value)
+            {
+                current.left = Add(current.left, value);
+            }
+            else
+            {
+                current.right = Add(current.right, value);
+            }
+
+            return current;
+        }
+
+        private bool contains(TreeNode current, int value)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+
+            if (value == current.value)
+            {
+                return true;
+            }
+
+            return value < current.value
+                ? contains(current.left, value)
+                : contains(current.right, value);
+
+        }
+
+        public bool contains(int value)
+        {
+            return contains(root, value);
+        }
+
+        public TreeNode Add(int value)
+        {
+            return Add(root, value);
+        }
+
+        private TreeNode delete(TreeNode current, int value)
+        {
+            if (current == null)
+            {
+                return null;
+            }
+
+            if (value == current.value)
+            {
+                if (current.left == null && current.right == null)
+                {
+                    return null;
+                }
+                if (current.left == null)
+                {
+                    return current.right;
+                }
+                if (current.right == null)
+                {
+                    return current.left;
+                }
+                int element = smallest(current.right);
+                current.value = element;
+                current.right = delete(current.right, element);
+                return current;
+            }
+
+            
+
+            if (value<current.value)
+            {
+                current.left = delete(current.left, value);
+                return current;
+            }
+
+            current.right = delete(current.right, value);
+            return current;
+        }
+
+        public TreeNode delete(int value)
+        {
+            return delete(root, value);
+        }
+
+        private int smallest(TreeNode node)
+        {
+            return node.left == null ? node.value : smallest(node.left);
+        }
+
+        private void printInorder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            printInorder(node.left);
+            Console.Write(node.value + " ");
+            printInorder(node.right);
+           
+        }
+        private void printPreorder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Console.Write(node.value + " ");
+            printPreorder(node.left);
+            printPreorder(node.right);
+           
+        }
+        private void printPostorder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            printPostorder(node.left);
+            printPostorder(node.right);
+            Console.Write(node.value + " ");
+
+        }
+
+        public void printInorder()
+        {
+            printInorder(this.root);
+        }
+        public void printPreorder()
+        {
+            printPreorder(this.root);
+        }
+        public void printPostorder()
+        {
+            printPostorder(this.root);
+        }
+
+        public void printInBreadthFirst()
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                var temp = queue.Dequeue();
+                Console.Write(temp.value+" ");
+                if (temp.left != null)
+                {
+                    queue.Enqueue(temp.left);
+                }
+                if (temp.right != null)
+                {
+                    queue.Enqueue(temp.right);
+                }
+            }
+        }
+        public void printInDepthFirst()
+        {
+            var queue = new Stack<TreeNode>();
+            queue.Push(root);
+            while (queue.Count != 0)
+            {
+                var temp = queue.Pop();
+                Console.Write(temp.value+" ");
+                if (temp.right != null)
+                {
+                    queue.Push(temp.right);
+                }
+                if (temp.left != null)
+                {
+                    queue.Push(temp.left);
+                }
+               
+            }
+        }
+        
+        public void pringInBreadthRec()
+        {
+            int h = height(root);
+            for (int i = 0; i <= h; i++)
+            {
+                pringInBreadthRec(root, i);
+            }
+        }
+
+        private int height(TreeNode treeNode)
+        {
+            if (treeNode == null)
+            {
+                return 0;
+            }
+            int lheight = height(treeNode.left);
+            int rheight = height(treeNode.right);
+
+            return lheight > rheight
+                ? lheight + 1
+                : rheight + 1;
+        }
+
+        public void pringInBreadthRec(TreeNode treeNode, int lvl)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (lvl == 1)
+            {
+                Console.Write(treeNode.value + " ");
+            }
+            else if (lvl > 1)
+            {
+                pringInBreadthRec(treeNode.left, lvl-1);
+                pringInBreadthRec(treeNode.right, lvl-1);
+            }
+        }
+
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Trees : IEnumerable<Tree>
+    {
+
+        public Tree[] trees;
+
+        public void sort()
+        {
+        }
+
+        public IEnumerator<Tree> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
